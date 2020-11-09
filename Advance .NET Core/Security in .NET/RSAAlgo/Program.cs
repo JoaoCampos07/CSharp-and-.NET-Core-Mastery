b9002cc59af1447cc89381c70d727b3631eea35e
JoaoPedroQCampos <joao.campos@itsector.pt>
2020-11-09 20:04:02 +0100

    [RSAEncryption] + Add: Second Part Decrypt using the private key.

--- Advance .NET Core/Security in .NET/RSAAlgo/Program.cs
+++ Advance .NET Core/Security in .NET/RSAAlgo/Program.cs
@@ -20,11 +20,20 @@ namespace IDisposableTraining
             var byteConverter = new UnicodeEncoding();
             
             // let's initialize our RSA algo with the private key
-            var privateKey = File.ReadAllText("..\\..\\..\\Certificates\\private_key.xml"); // I'm at bin folder, i need to get to the root and continue
-            rsa.FromXmlStringExtended(privateKey);
+            var publicKey = File.ReadAllText("..\\..\\..\\Certificates\\public_key.xml"); // I'm at bin folder, i need to get to the root and continue
+            rsa.FromXmlStringExtended(publicKey);
 
             var encodedInfo = byteConverter.GetBytes(privateInfo);
-            rsa.Encrypt(encodedInfo, RSAEncryptionPadding.OaepSHA256);
+            var encryptedInfo = rsa.Encrypt(encodedInfo, RSAEncryptionPadding.OaepSHA256);
+
+            // let's decoded this...
+
+            var privateKey = File.ReadAllText("..\\..\\..\\Certificates\\private_key.xml"); 
+            RSA rsa2 = RSA.Create();
+
+            rsa2.FromXmlStringExtended(privateKey);
+            var decryptedInfo = rsa2.Decrypt(encryptedInfo, RSAEncryptionPadding.OaepSHA256);
+            var privateInfo2 = byteConverter.GetString(decryptedInfo);
         }
     }
 }
