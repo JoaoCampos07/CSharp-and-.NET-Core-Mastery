c51a2e498f72ac1b21a18b832410f3b080aea8b4
JoaoPedroQCampos <joao.campos@itsector.pt>
2020-11-09 19:56:20 +0100

    [RSAEncryption] Create RSA algo obj. Encode string into Bytes. Read private key from file. Encrypt bytes with private key.

--- Advance .NET Core/Security in .NET/RSAAlgo/Program.cs
+++ Advance .NET Core/Security in .NET/RSAAlgo/Program.cs
@@ -1,16 +1,30 @@
 ﻿using System;
-
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Console;
 
 namespace IDisposableTraining
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            RSA rsa = RSA.Create();
 
+            string privateInfo = "Joao Campos";
+
+            // Create a UnicodeEncoder to convert string -> byte[]
+            var byteConverter = new UnicodeEncoding();
+            
+            // let's initialize our RSA algo with the private key
+            var privateKey = File.ReadAllText("..\\..\\..\\Certificates\\private_key.xml"); // I'm at bin folder, i need to get to the root and continue
+            rsa.FromXmlStringExtended(privateKey);
+
+            var encodedInfo = byteConverter.GetBytes(privateInfo);
+            rsa.Encrypt(encodedInfo, RSAEncryptionPadding.OaepSHA256);
         }
     }
 }
