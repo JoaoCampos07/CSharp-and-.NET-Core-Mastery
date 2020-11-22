18014d8ceea55672d0bd53b2cb3f3942aeac5f97
JoaoPedroQCampos <joao.campos@itsector.pt>
2020-11-22 19:47:52 +0100

    [Enumerator] + Add: Example : Convert in[] -> string using Enumerator.

---
+++ Advance Features C#/CSharp Features/IntArrayToString/Program.cs
@@ -0,0 +1,63 @@
+﻿/*
+ * https://stackoverflow.com/questions/1822811/int-array-to-string
+ */
+
+namespace IntArrayToString
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // I have a array of ints, containing digits only. I want to convert this array to string.
+            // How to do it ?
+
+            int[] arr = { 0, 1, 2, 3, 0, 1 };
+
+            FirstOption_Join(arr);
+
+            SecondOption_AggregateFunc(arr);
+
+            ThirdOption_ExtensionMethod(arr);
+
+            var enumerator = Enumerable.Range(0, 5).ToList().GetEnumerator();
+
+            while(enumerator.MoveNext())
+            {
+                Console.WriteLine($"{enumerator.Current}");
+            }
+        }
+
+        private static void ThirdOption_ExtensionMethod(int[] arr)
+        {
+            var result = arr.ToStringNonLinq3<int>("");
+            Console.WriteLine($" Result : {result}");
+        }
+
+        private static void SecondOption_AggregateFunc(int[] arr)
+        {
+            var result = arr.Aggregate(string.Empty, (s, i) => s + i.ToString());
+
+            Console.WriteLine($" Result : {result}");
+        }
+
+        private static void FirstOption_Join(int[] arr)
+        {
+            // with Linq
+            // 1. Convert to List<int> to easy manipulate 
+            // 2. Convert every element of this list to string
+            // 3. Convert again to array.
+            // 4. Join each element of this array to 
+            var list = new List<int>(arr);
+            var strlist = list.ConvertAll(i => i.ToString());
+            var finalArray = strlist.ToArray();
+
+            string result = string.Join("", finalArray);
+
+            Console.WriteLine($" Result : {result}");
+        }
+    }
+}
---
+++ Advance Features C#/CSharp Features/SimpleYieldExample/Program.cs
@@ -0,0 +1,41 @@
+﻿
+//https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/yield
+
+namespace SimpleYieldExample
+{
+    using System;
+    using System.Collections.Generic;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = Power(2, 8);
+
+            foreach (var i in result)
+            {
+                Console.WriteLine("{0}", i);
+            }
+        }
+
+        private static IEnumerable<int> Power(int number, int exponent)
+        {
+            int result = 1;
+
+            for (int i = 0; i < exponent; i++)
+            {
+                result = result * number;
+                yield return result;
+            }
+        }
+    }
+}
+
+// Each iteration in Foreach in Main() makes a call to the Power method
+
+// What the yield word do ? 
+// R: Indicates that the method where is used, is a Iterator. 
+// This removes a necessity for a extra class (A class that holds the state of the enumeration.)
+
+// Why we use the yield return statement ? 
+// R: We use it to return one element at time.
