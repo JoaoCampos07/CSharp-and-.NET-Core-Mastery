53cc629c01848e769435327c9efbb58b6d01eaa6
JoaoPedroQCampos <joao.campos@itsector.pt>
2020-11-22 11:02:44 +0100

    [Delegates] + Add: The problem, a framework that is not extensible. I cannot use filters.

---
+++ Advance Features C#/CSharp Features/MoshHamenadi/PhotoFilters.cs
@@ -0,0 +1,25 @@
+﻿using MoshHamenadi.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoshHamenadi
+{
+    public class PhotoFilters
+    {
+        public void ApplyBrightness(Photo photo)
+        {
+            Console.WriteLine("Apply Brightness");
+        }
+
+        public void ApplyContrast(Photo photo)
+        {
+            Console.WriteLine("Apply contrast");
+        }
+
+        public void Resize(Photo photo)
+        {
+            Console.WriteLine("Resize photo");
+        }
+    }
+}
---
+++ Advance Features C#/CSharp Features/MoshHamenadi/PhotoProcessor.cs
@@ -0,0 +1,15 @@
+﻿using MoshHamenadi.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoshHamenadi
+{
+    public class PhotoProcessor
+    {
+        public void Process(string path)
+        {
+            var photo = Photo.Load(path);
+        }
+    }
+}
