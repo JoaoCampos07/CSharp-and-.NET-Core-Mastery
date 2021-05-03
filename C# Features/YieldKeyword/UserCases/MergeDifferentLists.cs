0ab44eb3894291ecde18c120c98ec34ad93149df
João P. Campos <joaocampos07@outlook.com>
2021-05-03 12:55:02 +0200

    * Change: [Yield] Update the Documentation and examples on Lazy Iterators and the Yield keyword.

---
+++ C# Features/YieldKeyword/UserCases/MergeDifferentLists.cs
@@ -0,0 +1,73 @@
+﻿namespace YieldKeyword.UserCases
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class MergeDifferentLists
+    {
+        private List<CurrentAccount> CurrentAccountList { get; set; }
+        private List<SavingAccount> SavingAccountList { get; set; }
+
+
+        /// <summary>
+        /// Using a Enumerator and the Yield keyword.
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<IAccount> GetAccountNumbers() // O(x) + O(y) : x = ca ; y = sa ==> Better performance and i have Single Responsability
+        {
+            if(CurrentAccountList != null && CurrentAccountList.Any())
+            {
+                foreach (var ca in CurrentAccountList)
+                {
+                    yield return ca;
+                }
+            }
+
+            if(SavingAccountList != null && SavingAccountList.Any())
+            {
+                foreach (var sa in SavingAccountList)
+                {
+                    yield return sa;
+                }
+            }
+        }
+
+        public IEnumerable<string> GetAccountNumbersWithLINQ() // TOTAL : ( O(x) + O(y) ) + O(y)
+        {
+            var CANumbersList = CurrentAccountList.Select(ca => ca.Number).ToList(); // O(x) 
+            var SANumberList = SavingAccountList.Select(sa => sa.Number); // O(y) : x = ca ; y = sa
+
+            return CANumbersList.Concat(SANumberList); // Where I assume that they do append, so I assume is  a O(y)
+        }
+
+
+        #region Models
+        public interface IAccount
+        {
+            public string Number { get; set; }
+            public AccountType Type { get; set; }
+        }
+
+        public enum AccountType
+        {
+            CurrentAccount,
+            SavingAccount
+        }
+
+        private class SavingAccount : IAccount
+        {
+            public string Number { get; set; }
+            public AccountType Type { get; set; }
+        }
+
+        private class CurrentAccount : IAccount
+        {
+            public string Number { get; set; }
+            public AccountType Type { get; set; }
+        } 
+        #endregion
+    }
+}
---
+++ C# Features/YieldKeyword/UserCases/TwoSimpleOperations.cs
@@ -0,0 +1,53 @@
+﻿namespace YieldKeyword.UserCases
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class TwoSimpleOperations
+    {
+        public void DisplayPowerOfTwo()
+        {
+            // TOTAL : O(n^2)
+            // ...first operation i compute all the power of two
+            var numbers = PowerWithoutYield(2, 8).ToArray(); // O(n)
+
+            for (int i = 0; i < numbers.Length; i++) // O(n) 
+            {
+                Console.WriteLine(numbers[i]); //second operation i print them
+            }
+
+
+            // with Yield and Lazy Enumerator => TOTAL : O(n) with Single Responsability being respected.
+            foreach (var i in Power(2, 8))
+            {
+                Console.WriteLine("{0}", i); // second operation
+            }
+        }
+
+        public static IEnumerable<int> PowerWithoutYield(int number, int exponent)
+        {
+            var powerOfTwo = new int[numbers.Length];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                powerOfTwo[i] = numbers[i] * numbers[i];
+            }
+
+            return powerOfTwo;
+        }
+
+        private static IEnumerable<int> Power(int number, int exponent)
+        {
+            int result = 1;
+
+            for (int i = 0; i < exponent; i++)
+            {
+                result = result * number;
+                yield return result;
+            }
+        }
+    }
+}
